d0114a41fd6c04fecc456c350439547a22703081
Andrew Red <andrew@reddikh.com>
2018-04-21 12:50:31 +0300

    fixed version name for published image
    :goose:

--- docker/publish.sh
+++ docker/publish.sh
@@ -6,8 +6,8 @@ environmentVersion=`npm show goose-${environmentName}-environment version`;
 
 echo "Building image for $1";
 IMAGE_NAME="redcode/goose-phantom-environment";
-TAG_NAME_VERSIONED="${environmentName}-${environmentVersion}";
-TAG_NAME_LATEST="${environmentName}-latest";
+TAG_NAME_VERSIONED="${environmentVersion}";
+TAG_NAME_LATEST="latest";
 docker build --build-arg ENVIRONMENT=$environment -t "$TAG_NAME_VERSIONED" -t "$TAG_NAME_LATEST" -f ./Dockerfile .
 DOCKER_NAME_VERSIONED="$IMAGE_NAME:$TAG_NAME_VERSIONED"
 DOCKER_NAME_LATEST="$IMAGE_NAME:$TAG_NAME_LATEST"
