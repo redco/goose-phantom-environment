5af9cc65271f270f4a5f4c96fadf30cb4a18884e
Andrew Red <andrew@reddikh.com>
2018-05-19 12:31:20 +0300

    Updated publish.sh script
    :goose:

--- docker/publish.sh
+++ docker/publish.sh
@@ -6,13 +6,11 @@ environmentVersion=`npm show goose-${environmentName}-environment version`;
 
 echo "Building image for $1";
 IMAGE_NAME="redcode/goose-${environmentName}-environment";
-TAG_NAME_VERSIONED="${environmentVersion}";
-TAG_NAME_LATEST="latest";
+TAG_NAME_VERSIONED="${IMAGE_NAME}:${environmentVersion}";
+TAG_NAME_LATEST="${IMAGE_NAME}:latest";
 docker build --build-arg ENVIRONMENT=$environment -t "$TAG_NAME_VERSIONED" -t "$TAG_NAME_LATEST" -f ./Dockerfile .
-DOCKER_NAME_VERSIONED="$IMAGE_NAME:$TAG_NAME_VERSIONED"
-DOCKER_NAME_LATEST="$IMAGE_NAME:$TAG_NAME_LATEST"
-docker tag "$TAG_NAME_VERSIONED" "$DOCKER_NAME_VERSIONED"
-docker tag "$TAG_NAME_LATEST" "$DOCKER_NAME_LATEST"
-docker push "$DOCKER_NAME_VERSIONED"
-docker push "$DOCKER_NAME_LATEST"
-docker rmi "$DOCKER_NAME_VERSIONED" "$DOCKER_NAME_LATEST"
+docker tag "$TAG_NAME_VERSIONED" "$TAG_NAME_VERSIONED"
+docker tag "$TAG_NAME_LATEST" "$TAG_NAME_LATEST"
+docker push "$TAG_NAME_VERSIONED"
+docker push "$TAG_NAME_LATEST"
+docker rmi "$TAG_NAME_VERSIONED" "$TAG_NAME_VERSIONED"
